c9c101ca1a9aace75c4fcb571b7c54833bf57f16
Perwez Mallick <mallickperwez43@gmail.com>
2024-07-13 15:34:00 +0530

    Create 01_Intro_to_JS.js

---
+++ 01_Intro_to_JS.js
@@ -0,0 +1,10 @@
+console.log("This program adds 1, 2 and 54 nos")
+
+// function to add three nos
+function addThreeNumbers(a, b, c) {
+  return a + b + c;
+}
+
+// printing it now
+let c = addThreeNumbers(1, 2, 54)
+console.log(c)
