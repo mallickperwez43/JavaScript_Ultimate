2db0fdda0ce5c7424395871fb13e06fdc913de8d
Perwez Mallick <mallickperwez43@gmail.com>
2024-07-13 15:46:42 +0530

    Create 02_Variables.js

---
+++ 02_Variables.js
@@ -0,0 +1,24 @@
+// JavaScript is a Dynamically Typed Language
+// JavaScrpit allows us to change the type of a variable at runtime
+
+// Variables : it is a container that store a particular value/data in memory
+
+// printing
+console.log(
+    "This repl contains code for the Ultimate JavaScript course video no 2",
+  );
+  
+  let a = 67; // a contains 67
+  console.log(a);
+  a = "harry"; // changing int to string at runtime
+  console.log(a);
+  a = "s"; // changing string to char at runtime
+  console.log(a);
+  
+  // DECLARATION RULES
+  
+  // format -> data_type identifire = literal(value)
+  // 1] Letters, digits, underscores and $ are allowed
+  // 2] Must begin with a $, _ or a letter
+  // 3] JavaScrpit resevered words cannot be used as a varibale name
+  // 4] Harry and haRRy are different variables (case sensitive)
