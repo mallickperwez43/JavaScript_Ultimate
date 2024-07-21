8e2206e2418bb642dbbdc449f260a83bb8166eff
Perwez Mallick <mallickperwez43@gmail.com>
2024-07-21 19:27:01 +0530

    Create 13_Strings.js

---
+++ 13_Strings.js
@@ -0,0 +1,44 @@
+const propmt = require("prompt-sync")();
+/*
+// Strings are used to store and manupilate text
+
+// strings can be created using double quote as well as single quote
+let name = "Harry";
+console.log("Name is : " + name + " and length is : " + name.length);
+let friend = 'Prakash';
+console.log("Friend's name is : " + friend + " and length is : " + friend.length);
+// let myFriend = "Somebody'   -> dont do this quoatation mistake
+
+// index is 0 based for string
+
+// Template Literals
+let boy1 = "Pramod";
+let boy2 = "Nikhil";
+// TO PRINT -> Nikhil is a friend os Pramod
+// use backtick instead of quotes to define a string -> template literal 
+let sentence = `boy2 is a friend of boy1`;   // printing the exact same string 
+console.log(sentence);
+console.log("Printing the actual string without changes");
+sentence = `${boy2} is a friend of ${boy1}`;   // string interpolation -> replace the value of the variable 
+console.log(sentence);
+console.log("Printing the variables value in that string");
+console.log(`Friend's name is ${friend}`); // string interpolation -> inserting the values of the variable friend 
+
+// Escape Sequence Characters
+
+// let name1 = 'Adam D'Angelo';  // java srcipt will get confused
+let myName = 'Adam D\'Angelo';     
+let quote = "\"Its the best and it will always be\"";     
+// BackSlash(\ Escape Sequence -> after backslash the character is considered as the readable)
+console.log(myName, myName.length); 
+console.log(quote, quote.length); 
+*/
+// \n -> newline
+// \t -> tab
+// \r -> carriage return
+//let para = "Hello Brother!!\nHow are you?";
+//console.log(para);
+//let para2 = "Hello Brother!!\tHow are you?";
+//console.log(para2);
+let para3 = "Hello Brother!!\r How are you?";
+console.log(para3);
