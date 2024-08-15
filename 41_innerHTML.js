826fe01357a94cb01b9b012eda414e90bd64d5e2
Perwez Mallick <mallickperwez43@gmail.com>
2024-08-15 23:26:25 +0530

    Create 41_innerHTML.js

---
+++ 41_innerHTML.js
@@ -0,0 +1,15 @@
+// let x = document.getElementsByTagName('span')[0];
+// console.log(x);
+// let y = document.getElementsByTagName('span')[0];
+// console.dir(y);
+
+// console.log(document.body.firstChild.nodeName);
+// console.log(document.body.firstElementChild.nodeName);
+
+// first.innerHTML;
+// first.innerHTML = <i>Hey I am Italic</i>;
+// first.outerHTML;
+// first.outerHTML = <div>Hey</div>;
+// document.body.firstChild.data
+// console.log(document.body.textContent);
+// first.hidden = false
