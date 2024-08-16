ab7f76d940fc38189afaec8a33e2541f94d2cfb0
Perwez Mallick <mallickperwez43@gmail.com>
2024-08-16 23:50:47 +0530

    Create 45_classname_classlist.js

---
+++ 45_classname_classlist.js
@@ -0,0 +1,9 @@
+// write about className and classList
+
+let first = document.getElementById("first");       
+first.className = "text-dark red";
+first.classList.add("yellow");
+// first.classList.toggle("red");
+// first.classList.toggle("yellow");
+// first.classList.contains("red");
+// first.classList.contains("yellow");
