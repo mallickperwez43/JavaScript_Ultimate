5f73e8aada7ff8d4266366f7f6b4fcf901278fd8
Perwez Mallick <mallickperwez43@gmail.com>
2024-08-28 21:40:35 +0530

    Create 64_Exercise4_Solution.js

---
+++ 64_Exercise4_Solution.js
@@ -0,0 +1,10 @@
+setInterval(() => {
+
+    let date = new Date();
+    let options = {
+        weekday: "long", year: "numeric", month: "short",
+        day: "numeric", hour: "2-digit", minute: "2-digit"
+    };
+    let d = date.toLocaleDateString("en-us", options)
+    time.innerHTML = d;
+}, 1000)
