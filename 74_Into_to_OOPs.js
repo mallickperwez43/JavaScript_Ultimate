4d50592b7c65e9a0efc1f2690eef7dd8d938cbfc
Perwez Mallick <mallickperwez43@gmail.com>
2025-08-02 16:39:47 +0530

    Create 74_Into_to_OOPs.js

---
+++ 74_Into_to_OOPs.js
@@ -0,0 +1,25 @@
+// Object Oriented Programming
+
+// Based on the concepts of "objects" where we create an object which have methods and properites
+// Better for code reuseability and creating a method or a class and using it again and again
+
+// 4 Pillars
+
+// 1] Abstarction
+// Hiding of internal details and showing on essential features
+// eg : arr.sort() -> sort() karega kaise karega usse matlab nhi
+
+// 2] Encapsulation
+// Bundling properties and methods in a single Unit
+// eg : STL -> sari properties hoti
+
+// 3] Inheritance
+// A new class can reuse and extend properties of old class
+// eg : game : characters(health, power)
+// hero bhi character hai aur villian bhi
+
+// 4] Polymorphism
+// Ability of an object or function to take many forms
+// eg : Shapes(class)
+// rectangle(shape ki properties and methods lega)
+// triangle(shape ki properties and methods lega)
