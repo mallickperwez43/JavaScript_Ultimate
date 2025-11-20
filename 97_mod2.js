0ab6ed7f1bc27046564297ac2c326ff6ac1aa245
Perwez Mallick <mallickperwez43@gmail.com>
2025-11-20 20:43:27 +0530

    Create 97_mod2.js

---
+++ 97_mod2.js
@@ -0,0 +1,11 @@
+// ES6 Modules
+
+export const hello_Mod_Two = () => {
+    console.log("Hello Harry");
+    console.log("Hello I came from file 97_mod2.js & I am hello function");
+}
+
+export const advHello_Mod_Two = (name) => {
+    console.log(`Hello ${name}`);
+    console.log("Hello I came from file 97_mod2.js & I am advance hello function");
+}
